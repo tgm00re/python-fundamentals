7ef1938523f360f55de0375ee784b5e0b3aabe8a
Thomas Moore <tmoorecode@gmail.com>
2022-02-04 17:48:10 -0500

    changed  to chained methods

--- user/user.py
+++ user/user.py
@@ -5,15 +5,18 @@ class User:
 
     def deposit(self, amount):
         self.balance += amount
+        return self
 
     def withdraw(self, amount):
         if(amount <= self.balance):
             self.balance -= amount
         else:
             print("Sorry, not enough balance!")
+        return self
 
     def display_balance(self):
         print("balance: " + str(self.balance))
+        return self
 
     def transfer_balance(self, user, amount):
         if(amount <= self.balance):
@@ -21,14 +24,13 @@ class User:
             self.balance -= amount
         else:
             print("sorry, you don't have that much!")
+        return self
 
 jerry = User()
-jerry.deposit(100)
-jerry.display_balance()
-jerry.withdraw(200)
+jerry.deposit(100).display_balance().withdraw(200)
 
 kathy = User()
-kathy.deposit(500)
+kathy.deposit(500).transfer_balance(jerry, 300)
 kathy.transfer_balance(jerry, 300)
 
 jerry.display_balance()
