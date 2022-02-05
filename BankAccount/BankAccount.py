79e79c15ecfa2e666bd2c10f560a521d434c737d
Thomas Moore <tmoorecode@gmail.com>
2022-02-05 12:54:54 -0500

    added code from user file  to the bank account file

--- BankAccount/BankAccount.py
+++ BankAccount/BankAccount.py
@@ -17,6 +17,9 @@ class BankAccount:
         else:
             print("Insufficnet funds: Charging a $5 fee and deduct $5")
         return self
+
+    def getBalance(self):
+        return self.balance
         
     
     def display_account_info(self):
@@ -31,15 +34,68 @@ class BankAccount:
         for acc in cls.accounts:
             print(acc.balance)
 
+    @staticmethod
+    def hasEnough(amount, balance):
+        if(amount <= balance):
+            return True
+        return False
 
 
 
-acc1 = BankAccount(balance=5)
-acc2 = BankAccount(0.05)
+class User:
+    def __init__(self):
+        self.savings = BankAccount(.05, 500)
+        self.spendings = BankAccount(.01, 250)
 
-acc1.deposit(20).deposit(25).deposit(25).yield_interest().display_account_info()
+    def deposit(self, amount, accountType):
+        if(accountType == "spendings"):
+            self.spendings.deposit(amount)
+        elif(accountType == "savings"):
+            self.savings.deposit(amount)
+        return self
 
-acc2.deposit(100).deposit(100).withdraw(10).withdraw(20).withdraw(50).withdraw(5).yield_interest().display_account_info()
+    def withdraw(self, amount, accountType):
+        if(accountType == "spendings"):
+            self.spendings.withdraw(amount)
+        elif(accountType == "savings"):
+            self.savings.withdraw(amount)
+        return self
 
-BankAccount.displayAccountsInfo()
+    def display_balance(self, accountType):
+        if(accountType == "spendings"):
+            self.spendings.display_account_info()
+        elif(accountType == "savings"):
+            self.savings.display_account_info()
+        return self
 
+    def transfer_balance(self, user, amount, accountType, accountType2):
+        if(accountType == "spendings"):
+            if(self.spendings.hasEnough(amount, self.spendings.getBalance())):
+                self.spendings.withdraw(amount)
+                if(accountType2 == "spendings"):
+                    user.spendings.deposit(amount)
+                elif(accountType2 == "savings"):
+                    user.savings.deposit(amount)
+            else:
+                print("sorry, not enough money :(")
+        elif(accountType == "savings"):
+            if(self.savings.hasEnough(amount, self.savings.getBalance())):
+                self.savings.withdraw(amount)
+                if(accountType2 == "spendings"):
+                    user.spendings.deposit(amount)
+                elif(accountType2 == "savings"):
+                    user.savings.deposit(amount)
+            else:
+                print("sorry, not enough money :(")
+        
+        return self
+
+user1 = User()
+user2 = User()
+
+user1.deposit(500, "spendings")
+user1.display_balance("spendings")
+user1.display_balance("savings")
+
+user2.transfer_balance(user2, 100, "spendings", "savings")
+user2.display_balance("savings")
